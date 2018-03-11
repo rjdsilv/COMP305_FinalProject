144d2701d0a77506bedd68a2b73ae7796a585742
Rodrigo Januario da Silva <rjdsilv@gmail.com>
2018-03-11 15:33:13 -0400

    Created the First House

--- Assets/Scripts/Controller/Scenario/VillageController.cs
+++ Assets/Scripts/Controller/Scenario/VillageController.cs
@@ -1,16 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class VillageController : MonoBehaviour {
+public class VillageController : MonoBehaviour
+{
+    private Dictionary<GameObject, int> colliderDictionary = new Dictionary<GameObject, int>();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!colliderDictionary.ContainsKey(collision.gameObject))
+        {
+            colliderDictionary.Add(collision.gameObject, collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder);
+            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = GetComponent<SpriteRenderer>().sortingOrder - 1;
+            Debug.Log(collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder);
+        }
+
+        Color oldColor = GetComponent<SpriteRenderer>().color;
+        GetComponent<SpriteRenderer>().color = new Color(oldColor.r, oldColor.g, oldColor.b, 0.5f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (colliderDictionary.ContainsKey(collision.gameObject))
+        {
+            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = colliderDictionary[collision.gameObject];
+            colliderDictionary.Remove(collision.gameObject);
+            Debug.Log(collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder);
+        }
+
+        Color oldColor = GetComponent<SpriteRenderer>().color;
+        GetComponent<SpriteRenderer>().color = new Color(oldColor.r, oldColor.g, oldColor.b, 1.0f);
+    }
 }
