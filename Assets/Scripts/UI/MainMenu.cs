f2c943eda53ce2d68567096c575286843e7405b5
Rodrigo Januario da Silva <rjdsilv@gmail.com>
2018-04-01 17:07:39 -0400

    Built game and fixed item bug

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEditor;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
