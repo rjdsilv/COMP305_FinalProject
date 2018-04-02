31e3383842cb07f1ed6f4449feb7550dae82ab92
jalpendesai <djalpen24@gmail.com>
2018-04-02 01:05:15 -0400

    Added Splash Screen SFX

--- Assets/Scripts/UI/SplashScreen.cs
+++ Assets/Scripts/UI/SplashScreen.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SplashScreen : MonoBehaviour
 {
     public RectTransform textLogo;
     public RectTransform imageLogo;
+    //public Image imageLogo;
 
     private float startScale;
     private float endScale;
@@ -15,18 +17,25 @@ public class SplashScreen : MonoBehaviour
 	void Start ()
     {
         startScale = 0.25f;
-        endScale = 2f;
+        endScale = 1.5f;
         StartCoroutine(ScaleLogo());
 	}
 
     IEnumerator ScaleLogo()
     {
+        //while (startScale < endScale)
+        //{
+        //    textLogo.localScale = new Vector3(startScale, startScale, startScale);
+        //    imageLogo.localScale = new Vector3(startScale, startScale, startScale);
+        //    yield return new WaitForSeconds(increaseFactor);
+        //    startScale += increaseFactor;
+        //}
         while (startScale < endScale)
         {
             textLogo.localScale = new Vector3(startScale, startScale, startScale);
             imageLogo.localScale = new Vector3(startScale, startScale, startScale);
             yield return new WaitForSeconds(increaseFactor);
-            startScale += increaseFactor;
+            startScale += increaseFactor + 0.02f;
         }
         yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("GameIntroduction");
